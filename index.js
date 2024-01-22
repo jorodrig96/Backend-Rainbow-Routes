1649f203414a59ae96337b11bb4d9b5fa48e9f1e
jorodrig96 <jo.rodrig96@gmail.com>
2024-01-21 18:36:27 -0800

    created an express server with 2 different routes. one is the white color home page route and the other is accessing any color via thecoolor route with params

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">/home route</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
+})
+
+//using req.params to call any color you want 
+//in line 24, the charat method changed the first word, aka (0), to uppercase.
+app.get('/:color', (req, res) => {
+    let myColor = req.params.color
+    res.send(`<body style="margin: 0;">
+    <div>
+        <h2 style="text-align: center; color: ${myColor}">/:color route</h2>
+    </div>
+    <h1 style="border: 1px solid black; height: 10vh; background-color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)}page</h1>
+</body>`)
+})
+
+
+app.listen(process.env.PORT , () => {
+    console.log(` you have been connected to the app. running on ${process.env.PORT}`)
+})
